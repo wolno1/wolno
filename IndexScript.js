e6776387c2a43528ebfae2cf402480dde3b9e85f
angel <angelhernandezmn@gmail.com>
2025-01-19 00:02:19 -0600

    updated top bar

--- IndexScript.js
+++ IndexScript.js
@@ -116,12 +116,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function generateMenuItems() {
     var menuItems = [
-        { href: "index.html", text: "Home" },
-        { href: "about.html", text: "About me" },
-        { href: "portafolio.html", text: "Portfolio" },
-        { href: "chars.html", text: "Characters" },
-        { href: "comic.html", text: "Comic" },
-        { href: "faqs.html", text: "FAQs" }
+        { href: "Index.html", text: "Home" },
+        { href: "About.html", text: "About me" },
+        { href: "Portfolio.html", text: "Portfolio" },
+        { href: "Characters.html", text: "Characters" },
+        { href: "Comics.html", text: "Comics" },
+        { href: "FAQs.html", text: "FAQs" }
     ];
 
     var menu = document.getElementById("mySidenav");
